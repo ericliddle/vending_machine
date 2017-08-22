2663aa67b67d7fb93b833610bc99a058896de00b
heyliddle <eric.liddle@gmail.com>
2017-08-21 22:04:22 -0700

    pushing changes, 10/10 tests are passing

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -7,14 +7,6 @@ describe('vendingMachine', () => {
     vendor = new vendingMachine;
   })
 
-  describe('check on current stock', () => {
-    it('prints out the current stock for each item', () => {
-      const received = vendor.checkInventory();
-      expect(received).toMatchObject(received);
-    })
-  })
-
-
   describe('check on current items stock', () => {
     it('prints out the current stock for each item', () => {
       const received = vendor.checkItems();
@@ -22,9 +14,72 @@ describe('vendingMachine', () => {
     })
   })
 
-  describe('buy a bag of chips, gives correct change', () => {
-    it(, () => {
-      
+  describe('check on current coins in machine', () => {
+    it('returns total value for all coins in machine', () => {
+      const received = vendor.checkCoins();
+      expect(received);
     })
   })
+
+  describe('check if item can be purchahsed if there\'s enough money', () => {
+    it('should dispense item if value is >= item cost', () => {
+      const received = vendor.dispenseItem(5.00, 'chocolate bar');
+      expect(received).toEqual('You bought a chocolate bar.')
+    })
+    it('should not dispense the item if value is < item cost', () => {
+      const received = vendor.dispenseItem(0.50, 'nutts');
+      expect(received).toEqual('Insufficient cash, bitch.')
+    })
+    it('should not dispense if item is out of stock', () => {
+      const received = vendor.dispenseItem(4.00, 'cookies');
+      expect(received).toEqual('We out.')
+    })
+  })
+
+  describe('checks on the current stock of items in machine', () => {
+    it('will determine if 3 or more items are below min-count', () => {
+      const received = vendor.checkItemsStock();
+      const expectation = 3;
+      expect(received).toEqual(expectation);
+    })
+  })
+
+  describe('checks on the current amount of coins', () => {
+    it('will tally how many are less than 20', () => {
+      const received = vendor.checkCashOnHand();
+      const expectation = 4;
+      expect(received).toEqual(expectation);
+    })
+    it('will take all coins under 20 and top up to 50', () => {
+      vendor.refillCoins();
+      const received = vendor.checkCashOnHand();
+      const expectation = 0;
+      expect(received).toEqual(expectation);
+
+    })
+  })
+
+  describe('if item count is below min-count', () => {
+    it('will restock items to max-count', () => {
+      vendor.restockItems();
+      const received = vendor.checkItemsStock();
+      const expectation = 0;
+      expect(received).toEqual(expectation)
+    })
+  })
+
+  describe('returning purchasers change', () => {
+    it('will return change using highest value coins possible', () => {
+      const received = vendor.returnChange(2.85);
+      const expectation = {
+        twonie: 1,
+        loonie: 0,
+        quarter: 3,
+        dime: 1,
+        nickel: 0
+      }
+      expect(received).toEqual(expectation);
+    })
+  })
+
 })
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,7 +2,7 @@ let change = [
   {
     coin: "nickel",
     value: 0.05,
-    count: 18
+    count: 30
   },
   {
     coin: "dime",
@@ -30,28 +30,28 @@ items = [
   {
     item: "pop",
     price: 1.5,
-    count: 6,
+    count: 2,
     minCount: 5,
     maxCount: 15
   },
   {
     item: "chips",
     price: 1.25,
-    count: 8,
+    count: 1,
     minCount: 5,
     maxCount: 15
   },
   {
     item: "cookies",
     price: 3,
-    count: 10,
+    count: 0,
     minCount: 5,
     maxCount: 15
   },
   {
     item: "chocolate bar",
     price: 2.5,
-    count: 4,
+    count: 6,
     minCount: 5,
     maxCount: 15
   },
@@ -66,19 +66,69 @@ items = [
 
 class vendingMachine {
 
-  checkInventory() {
-    return items
-    console.log
-  };
-
   checkItems() {
     for (let i = 0; i < items.length; i++) {
-      console.log(items[i].item, change[i].coin)
+      console.log(items[i].item, "amount: " + items[i].count)
     }
   };
 
-  buyItems(item, quantity) {
-    
+  checkCoins() {
+    for (let i = 0; i < change.length; i++) {
+      console.log(change[i].coin, "total: $" + change[i].value * change[i].count)
+    }
+  };
+
+  restockItems() {
+    items.forEach(item => {
+      if (item.count < item.minCount) {
+        item.count = item.maxCount;
+      }
+    })
+  };
+
+  checkItemsStock() {
+    let lowStockItems = items.reduce((lowStockCount, item) => (
+      lowStockCount + (item.count < item.minCount)
+    ), 0)
+    return lowStockItems;
+  };
+
+  checkCashOnHand() {
+    let lowCash = change.reduce((lowCashCount, coin) => (
+      lowCashCount + (coin.count < 20)
+    ), 0)
+    return lowCash;
+  };
+
+  refillCoins() {
+    change.forEach(coin => {
+      if (coin.count < 20) {
+        coin.count = 50;
+      }
+    })
+  };
+
+  dispenseItem(cashPaid, snack) {
+    let itemPurchase = items.filter(item => (
+      item.item === snack
+    ))[0]
+    if (cashPaid < itemPurchase.price) {
+      return "Insufficient cash, bitch.";
+    } else if (itemPurchase.count === 0) {
+      return "We out."
+    } else {
+      return "You bought a " + snack + ".";
+    }
+  }
+
+  returnChange(cashBack) {
+    return {
+      twonie: parseInt(cashBack / 2),
+      loonie: parseInt((cashBack % 2) / 1),
+      quarter: parseInt((cashBack % 1) / 0.25),
+      dime: parseInt((cashBack % 0.25) / 0.10),
+      nickel: parseInt((cashBack % 0.10) / 0.05)
+    }
   }
 
 }
